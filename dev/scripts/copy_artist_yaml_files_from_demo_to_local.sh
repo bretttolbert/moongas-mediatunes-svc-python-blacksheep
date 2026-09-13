464bc470bf1b6f4e7afa74d1769dbc4454418a5b
Brett Tolbert <bretttolbert@gmail.com>
2026-09-13 12:28:57 -0500

    copy artists script wip

--- dev/scripts/copy_artist_yaml_files_from_demo_to_local.sh
+++ dev/scripts/copy_artist_yaml_files_from_demo_to_local.sh
@@ -4,6 +4,7 @@ set -euo pipefail
 copy-medialib \
   --src-paths "$MOONGAS_COLLECTION_DEMO/data/Music" \
   --dst-path "$MOONGAS_COLLECTION_LOCAL/data/" \
-  --include-filenames artist.yaml \
+  --include-filenames artist.yml \
   --dir-copy-mode PreserveStructure \
+  --overwrite-existing \
   --dry-run
